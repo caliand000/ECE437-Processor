013628e4a2335b2fdeb959e053768b846802be22
Andrew Cali <acali@purdue.edu>
2025-02-12 18:06:20 -0500

    hazard unit tests checkoff

--- asmFiles/test.datahazard.asm
+++ asmFiles/test.datahazard.asm
@@ -4,13 +4,12 @@
 org 0x0000
 
 ori   $3, $0, 0x5
-ori   $2, $0, 0x4
-
-add   $1, $2, $3            #using x1 in WB
-sub   $4, $1, $3            #using x1 in ID/RF
-and   $6, $1, $7            #using x1 in ID/RF
-or    $8, $1, $9            #using x1 in ID/RF
-XOR   $10, $1, $11          #using x1 in OD/RF 
+sw    $3, 4($0)  
+nop
+lw    $5, 4($0)
+add   $6, $5, $3
+sw    $6, 8($0)
 
 HALT
 
+#do a load then use value right after
