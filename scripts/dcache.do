96040f08b27bf1f4501b11f08e75820adb1ff85d
Andrew Cali <acali@purdue.edu>
2025-03-11 11:46:01 -0400

    fixes to dcache testbench and formatting in dcache.sv

---
+++ scripts/dcache.do
@@ -0,0 +1,47 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate /dcache_tb/CLK
+add wave -noupdate /dcache_tb/CPUCLK
+add wave -noupdate /dcache_tb/nRST
+add wave -noupdate /dcache_tb/check_output/case_info
+add wave -noupdate -divider Inputs
+add wave -noupdate /dcache_tb/dcif/halt
+add wave -noupdate /dcache_tb/dcif/dmemREN
+add wave -noupdate /dcache_tb/dcif/dmemWEN
+add wave -noupdate /dcache_tb/dcif/dmemstore
+add wave -noupdate /dcache_tb/dcif/dmemaddr
+add wave -noupdate -divider Outputs
+add wave -noupdate /dcache_tb/dcif/dhit
+add wave -noupdate /dcache_tb/dcif/flushed
+add wave -noupdate /dcache_tb/dcif/dmemload
+add wave -noupdate /dcache_tb/DUT/state
+add wave -noupdate /dcache_tb/DUT/cur_dcache
+add wave -noupdate /dcache_tb/DUT/halt_cnt
+add wave -noupdate /dcache_tb/DUT/hit_cnt
+add wave -noupdate /dcache_tb/DUT/index
+add wave -noupdate -divider {RAM connections}
+add wave -noupdate /dcache_tb/cif1/dwait
+add wave -noupdate /dcache_tb/cif1/dREN
+add wave -noupdate /dcache_tb/cif1/dWEN
+add wave -noupdate /dcache_tb/cif1/dload
+add wave -noupdate /dcache_tb/cif1/dstore
+add wave -noupdate /dcache_tb/cif1/daddr
+add wave -noupdate /dcache_tb/RAM/rstate
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {119605 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 150
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 1
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {76800 ps} {140800 ps}
--- source/dcache.sv
+++ source/dcache.sv
@@ -21,286 +21,277 @@ module dcache (
     logic hit0,hit1,enable_hit_counter,hit,offset,enable_halt_counter;
     logic [2:0] index;
     word_t[1:0] read_block,nxt_read_block;
+
     assign offset=dcif.dmemaddr[2];
     assign index=dcif.dmemaddr[5:3];
     assign hit0=(cur_dcache[index].way[0].tag==dcif.dmemaddr[31:6]&&cur_dcache[index].way[0].valid);
     assign hit1=(cur_dcache[index].way[1].tag==dcif.dmemaddr[31:6]&&cur_dcache[index].way[0].valid);
+
     always_ff@(posedge CLK,negedge nRST) begin
-    if(!nRST) begin
-    cur_dcache<='0;
-    hit_cnt<='0;
-    state<=Idle;
-    read_block<='0;
-    halt_cnt<=0;
+      if(!nRST) begin
+         cur_dcache<='0;
+         hit_cnt<='0;
+         state<=Idle;
+         read_block<='0;
+         halt_cnt<=0;
+      end
+      else begin
+         cur_dcache<=nxt_dcache;
+         hit_cnt<=nxt_hit_cnt;
+         state<=nextstate;
+         read_block<=nxt_read_block;
+         halt_cnt<=nxt_halt_cnt;
+      end 
     end
-    else begin
-    cur_dcache<=nxt_dcache;
-    hit_cnt<=nxt_hit_cnt;
-    state<=nextstate;
-    read_block<=nxt_read_block;
-    halt_cnt<=nxt_halt_cnt;
-    end 
-    end
+
     assign hit=hit0||hit1;
+
     always_comb begin : hit_counter
-    nxt_hit_cnt=hit_cnt;
-    if(enable_hit_counter) nxt_hit_cnt=hit_cnt+1;
+      nxt_hit_cnt=hit_cnt;
+      if(enable_hit_counter) nxt_hit_cnt=hit_cnt+1;
     end
+
     always_comb begin: halt_counter
-    nxt_halt_cnt=halt_cnt;
-    if(enable_halt_counter) nxt_halt_cnt=halt_cnt+1;
-    
+      nxt_halt_cnt=halt_cnt;
+      if(enable_halt_counter) nxt_halt_cnt=halt_cnt+1;
     end
+
     always_comb begin : next_State_logic
-    case(state)
-    Idle: begin
-    nextstate=Idle;
-    if(dcif.halt) begin
-        nextstate=write_first_word;
-    end
-   
-    else if(!hit)begin
-    nextstate=read_first_word;
-    
-    end
-    
-    end
+      case(state)
+         Idle: begin
+            nextstate=Idle;
+            if(dcif.halt) begin
+               nextstate=write_first_word;
+            end
+            else if(!hit)begin
+            nextstate=read_first_word;
+            end
+         end
 
-    
-    read_first_word: begin
-    nextstate=read_first_word;
-    if(!cif.dwait)
-    nextstate=read_second_word;
-    end
-    read_second_word: begin
-    nextstate=read_second_word;
-    if(!cif.dwait&&dcif.dmemREN)
-    nextstate=nhitR_doneR;
-    else if(!cif.dwait&&dcif.dmemWEN)
-    nextstate=nhitW_doneR;
-    end
-    nhitR_doneR: begin
-
-    if((cur_dcache[index].way[0].dirty&&!cur_dcache[index].ru[0])||(cur_dcache[index].way[1].dirty&&!cur_dcache[index].ru[1])) 
-    nextstate=write_first_word;
-    else nextstate=nhitR_doneC;
-    
-    end
-
-    nhitW_doneR: begin
-   
-    if((cur_dcache[index].way[0].dirty&&!cur_dcache[index].ru[0])||(cur_dcache[index].way[1].dirty&&!cur_dcache[index].ru[1])) begin
-    nextstate=write_first_word;
-    end
-    else nextstate=nhitW_doneC;
-    end
-
-    write_first_word: begin
-    nextstate=write_first_word;
-    if(!cif.dwait) nextstate=write_second_word;
-    end
-
-    write_second_word: begin
-    nextstate=write_second_word;
-    if(!cif.dwait&&dcif.dmemREN)
-    nextstate=nhitR_doneC;
-    else if(!cif.dwait&&dcif.dmemWEN)
-    nextstate=nhitW_doneC;
-    else if(!cif.dwait&&dcif.halt) begin
-    nextstate=incrementing;
-    end
-    end
-    incrementing: begin
-    nextstate=write_first_word;
-    if(halt_cnt==15) nextstate=halt_cleaned;
-    end
-    nhitR_doneC: begin
-    nextstate=Idle;
-    end
-    nhitW_doneC: begin
-    nextstate=Idle;
-    end
-    halt_cleaned: begin
-    nextstate=halt_cleaned;
-    if(!cif.dwait) nextstate=Idle;
-    end
-    endcase
+         read_first_word: begin
+            nextstate=read_first_word;
+            if(!cif.dwait)
+            nextstate=read_second_word;
+         end
+         read_second_word: begin
+            nextstate=read_second_word;
+            if(!cif.dwait&&dcif.dmemREN)
+            nextstate=nhitR_doneR;
+            else if(!cif.dwait&&dcif.dmemWEN)
+            nextstate=nhitW_doneR;
+         end
+         nhitR_doneR: begin
+            if((cur_dcache[index].way[0].dirty&&!cur_dcache[index].ru[0])||(cur_dcache[index].way[1].dirty&&!cur_dcache[index].ru[1])) 
+            nextstate=write_first_word;
+            else nextstate=nhitR_doneC;
+         end
+         nhitW_doneR: begin
+            if((cur_dcache[index].way[0].dirty&&!cur_dcache[index].ru[0])||(cur_dcache[index].way[1].dirty&&!cur_dcache[index].ru[1])) begin
+            nextstate=write_first_word;
+            end
+            else nextstate=nhitW_doneC;
+         end
+         write_first_word: begin
+            nextstate=write_first_word;
+            if(!cif.dwait) nextstate=write_second_word;
+         end
+         write_second_word: begin
+            nextstate=write_second_word;
+            if(!cif.dwait&&dcif.dmemREN)
+            nextstate=nhitR_doneC;
+            else if(!cif.dwait&&dcif.dmemWEN)
+            nextstate=nhitW_doneC;
+            else if(!cif.dwait&&dcif.halt) begin
+            nextstate=incrementing;
+            end
+         end
+         incrementing: begin
+            nextstate=write_first_word;
+            if(halt_cnt==15) nextstate=halt_cleaned;
+         end
+         nhitR_doneC: begin
+            nextstate=Idle;
+         end
+         nhitW_doneC: begin
+            nextstate=Idle;
+         end
+         halt_cleaned: begin
+            nextstate=halt_cleaned;
+            if(!cif.dwait) nextstate=Idle;
+         end
+      endcase
     end
  always_comb begin : output_logic
- cif.dREN=0;
- cif.dWEN=0;
- cif.daddr=0;
- cif.dstore=0;
- dcif.dhit=0;
- dcif.dmemload=0;
- nxt_dcache=cur_dcache;
- nxt_read_block=read_block;
- enable_hit_counter=0;
- enable_halt_counter=0;
+   cif.dREN=0;
+   cif.dWEN=0;
+   cif.daddr=0;
+   cif.dstore=0;
+   dcif.dhit=0;
+   dcif.dmemload=0;
+   nxt_dcache=cur_dcache;
+   nxt_read_block=read_block;
+   enable_hit_counter=0;
+   enable_halt_counter=0;
 
- case(state)
- Idle: begin
- if(dcif.dmemREN) begin
- if(hit0) begin
- dcif.dmemload=nxt_dcache[index].way[0].data[offset];
- nxt_dcache[index].ru[0]=1;
- nxt_dcache[index].ru[1]=0;
- enable_hit_counter=1;
- end
- else if(hit1) begin
- dcif.dmemload=nxt_dcache[index].way[1].data[offset];
- nxt_dcache[index].ru[1]=1;
- nxt_dcache[index].ru[0]=0;
- enable_hit_counter=1;
- end
- if(hit)
- dcif.dhit=1;
- end
- else if(dcif.dmemWEN) begin
- if(hit0) begin
- nxt_dcache[index].way[0].data[offset]=dcif.dmemstore;
- nxt_dcache[index].way[0].dirty=1;
- nxt_dcache[index].ru[0]=1;
- nxt_dcache[index].ru[1]=0;
- enable_hit_counter=1;
- end
- else if(hit1) begin
- nxt_dcache[index].way[1].data[offset]=dcif.dmemstore;
- nxt_dcache[index].way[1].dirty=1;
- nxt_dcache[index].ru[0]=0;
- nxt_dcache[index].ru[1]=1;
- enable_hit_counter=1;
- end
- dcif.dhit=1;
- end
- end
- 
- 
- read_first_word: begin
-    cif.dREN=1;
-    cif.daddr={dcif.dmemaddr[31:3],0,dcif.dmemaddr[1:0]};
- end
+   case(state)
+      Idle: begin
+         if(dcif.dmemREN) begin
+         if(hit0) begin
+            dcif.dmemload=nxt_dcache[index].way[0].data[offset];
+            nxt_dcache[index].ru[0]=1;
+            nxt_dcache[index].ru[1]=0;
+            enable_hit_counter=1;
+         end
+         else if(hit1) begin
+            dcif.dmemload=nxt_dcache[index].way[1].data[offset];
+            nxt_dcache[index].ru[1]=1;
+            nxt_dcache[index].ru[0]=0;
+            enable_hit_counter=1;
+         end
+         if(hit)
+            dcif.dhit=1;
+         end
+         else if(dcif.dmemWEN) begin
+         if(hit0) begin
+            nxt_dcache[index].way[0].data[offset]=dcif.dmemstore;
+            nxt_dcache[index].way[0].dirty=1;
+            nxt_dcache[index].ru[0]=1;
+            nxt_dcache[index].ru[1]=0;
+            enable_hit_counter=1;
+         end
+         else if(hit1) begin
+            nxt_dcache[index].way[1].data[offset]=dcif.dmemstore;
+            nxt_dcache[index].way[1].dirty=1;
+            nxt_dcache[index].ru[0]=0;
+            nxt_dcache[index].ru[1]=1;
+            enable_hit_counter=1;
+         end
+         if(hit)
+            dcif.dhit=1;
+         end
+      end
+      read_first_word: begin
+         cif.dREN=1;
+         cif.daddr={dcif.dmemaddr[31:3],0,dcif.dmemaddr[1:0]};
+      end
 
- read_second_word: begin
-    cif.dREN=1;
-    cif.daddr={dcif.dmemaddr[31:3],1,dcif.dmemaddr[1:0]};
-    nxt_read_block[0]=cif.dload;
+      read_second_word: begin
+         cif.dREN=1;
+         cif.daddr={dcif.dmemaddr[31:3],1,dcif.dmemaddr[1:0]};
+         nxt_read_block[0]=cif.dload;
+      end
+      nhitR_doneR : begin
+         nxt_read_block[1]=cif.dload;
+      end
+      nhitW_doneR: begin
+         nxt_read_block[1]=cif.dload;
+      end
+      write_first_word: begin
+         if(dcif.halt) begin
+            cif.dWEN=1;
+            cif.daddr={nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].tag,halt_cnt[2:0],3'b000};
+            cif.dstore=nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].data[0];
+         end
+         else begin
+            cif.dWEN=1;
+            cif.daddr={dcif.dmemaddr[31:3],0,dcif.dmemaddr[1:0]};
+            if(nxt_dcache[index].way[0].dirty&&!nxt_dcache[index].ru[0])
+               cif.dstore=nxt_dcache[index].way[0].data[0];
+            else if(nxt_dcache[index].way[1].dirty&&!nxt_dcache[index].ru[1])
+               cif.dstore=nxt_dcache[index].way[1].data[0];
+         end
+      end
+      write_second_word: begin
+         if(dcif.halt) begin
+            cif.dWEN=1;
+            cif.daddr={nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].tag,halt_cnt[2:0],3'b100};
+            cif.dstore=nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].data[1];
+         end
+         else begin
+            cif.dWEN=1;
+            cif.daddr={dcif.dmemaddr[31:3],1,dcif.dmemaddr[1:0]};
+            if(nxt_dcache[index].way[0].dirty&&!nxt_dcache[index].ru[0])
+               cif.dstore=nxt_dcache[index].way[0].data[1];
+            else if(nxt_dcache[index].way[1].dirty&&!nxt_dcache[index].ru[1])
+               cif.dstore=nxt_dcache[index].way[1].data[0];
+         end
+      end
+      incrementing: begin
+         enable_halt_counter=1;
+      end
+      nhitW_doneC: begin
+         if(!nxt_dcache[index].ru[0]) begin
+            if(dcif.dmemaddr[2])begin
+               nxt_dcache[index].way[0].data[0]=read_block[0];
+               nxt_dcache[index].way[0].data[1]=dcif.dmemstore;
+            end
+            else begin
+               nxt_dcache[index].way[0].data[1]=read_block[1];
+               nxt_dcache[index].way[0].data[0]=dcif.dmemstore;
+            end
+            nxt_dcache[index].way[0].valid=1;
+            nxt_dcache[index].way[0].dirty=1;
+            nxt_dcache[index].ru[0]=1;
+            nxt_dcache[index].ru[1]=0;
+            nxt_dcache[index].way[0].tag=dcif.dmemaddr[31:6];
+         end
+         else if(!nxt_dcache[index].ru[1]) begin
+            if(dcif.dmemaddr[2])begin
+               nxt_dcache[index].way[1].data[0]=read_block[0];
+               nxt_dcache[index].way[1].data[1]=dcif.dmemstore;
+            end
+            else begin
+               nxt_dcache[index].way[1].data[1]=read_block[1];
+               nxt_dcache[index].way[1].data[0]=dcif.dmemstore;
+            end
+            nxt_dcache[index].way[1].valid=1;
+            nxt_dcache[index].way[1].dirty=1;
+            nxt_dcache[index].ru[0]=0;
+            nxt_dcache[index].ru[1]=1;
+            nxt_dcache[index].way[1].tag=dcif.dmemaddr[31:6];
+         end
+         dcif.dhit=1;
+      end
+      nhitR_doneC: begin
+         if(!nxt_dcache[index].ru[0]) begin
+            if(dcif.dmemaddr[2])begin
+               nxt_dcache[index].way[0].data[0]=read_block[0];
+               nxt_dcache[index].way[0].data[1]=dcif.dmemstore;
+            end
+            else begin
+               nxt_dcache[index].way[0].data[1]=read_block[1];
+               nxt_dcache[index].way[0].data[0]=dcif.dmemstore;
+            end
+            nxt_dcache[index].way[0].valid=1;
+            nxt_dcache[index].way[0].dirty=0;
+            nxt_dcache[index].way[0].tag=dcif.dmemaddr[31:6];
+            nxt_dcache[index].ru[0]=1;
+            nxt_dcache[index].ru[1]=0;
+         end
+         else if(!nxt_dcache[index].ru[1]) begin
+            if(dcif.dmemaddr[2])begin
+               nxt_dcache[index].way[1].data[0]=read_block[0];
+               nxt_dcache[index].way[1].data[1]=dcif.dmemstore;
+            end
+            else begin
+               nxt_dcache[index].way[1].data[1]=read_block[1];
+               nxt_dcache[index].way[1].data[0]=dcif.dmemstore;
+            end
+            nxt_dcache[index].way[1].valid=1;
+            nxt_dcache[index].way[1].dirty=0;
+            nxt_dcache[index].way[1].tag=dcif.dmemaddr[31:6];
+            nxt_dcache[index].ru[1]=1;
+            nxt_dcache[index].ru[0]=0;
+         end
+         dcif.dhit=1;
+      end
+      halt_cleaned: begin
+         cif.dWEN=1;
+         cif.daddr=32'h3100;
+         cif.dstore=halt_cnt;
+         
+      end
+   endcase
+      dcif.flushed=(state==halt_cleaned&&!cif.dwait);
  end
- nhitR_doneR : begin
- nxt_read_block[1]=cif.dload;
- end
- nhitW_doneR: begin
-     nxt_read_block[1]=cif.dload;
- end
- write_first_word: begin
-    if(dcif.halt) begin
-    cif.dWEN=1;
-    cif.daddr={nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].tag,halt_cnt[2:0],3'b000};
-    cif.dstore=nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].data[0];
-    end
-    else begin
-    cif.dWEN=1;
-    cif.daddr={dcif.dmemaddr[31:3],0,dcif.dmemaddr[1:0]};
-    if(nxt_dcache[index].way[0].dirty&&!nxt_dcache[index].ru[0])
-    cif.dstore=nxt_dcache[index].way[0].data[0];
-    else if(nxt_dcache[index].way[1].dirty&&!nxt_dcache[index].ru[1])
-    cif.dstore=nxt_dcache[index].way[1].data[0];
-    end
- end
- write_second_word: begin
-    if(dcif.halt) begin
-    cif.dWEN=1;
-    cif.daddr={nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].tag,halt_cnt[2:0],3'b100};
-    cif.dstore=nxt_dcache[halt_cnt[2:0]].way[halt_cnt[3]].data[1];
-    end
-    else begin
-    cif.dWEN=1;
-    cif.daddr={dcif.dmemaddr[31:3],1,dcif.dmemaddr[1:0]};
-    if(nxt_dcache[index].way[0].dirty&&!nxt_dcache[index].ru[0])
-    cif.dstore=nxt_dcache[index].way[0].data[1];
-    else if(nxt_dcache[index].way[1].dirty&&!nxt_dcache[index].ru[1])
-    cif.dstore=nxt_dcache[index].way[1].data[0];
- end
- end
- incrementing: begin
- enable_halt_counter=1;
- end
- nhitW_doneC: begin
-     if(!nxt_dcache[index].ru[0]) begin
-     if(dcif.dmemaddr[2])begin
-     nxt_dcache[index].way[0].data[0]=read_block[0];
-     nxt_dcache[index].way[0].data[1]=dcif.dmemstore;
-     end
-     else begin
-     nxt_dcache[index].way[0].data[1]=read_block[1];
-     nxt_dcache[index].way[0].data[0]=dcif.dmemstore;
-     end
-     nxt_dcache[index].way[0].valid=1;
-     nxt_dcache[index].way[0].dirty=1;
-     nxt_dcache[index].ru[0]=1;
-     nxt_dcache[index].ru[1]=0;
-     nxt_dcache[index].way[0].tag=dcif.dmemaddr[31:6];
-     end
-     else if(!nxt_dcache[index].ru[1]) begin
-     if(dcif.dmemaddr[2])begin
-     nxt_dcache[index].way[1].data[0]=read_block[0];
-     nxt_dcache[index].way[1].data[1]=dcif.dmemstore;
-     end
-     else begin
-     nxt_dcache[index].way[1].data[1]=read_block[1];
-     nxt_dcache[index].way[1].data[0]=dcif.dmemstore;
-     end
-     nxt_dcache[index].way[1].valid=1;
-     nxt_dcache[index].way[1].dirty=1;
-     nxt_dcache[index].ru[0]=0;
-     nxt_dcache[index].ru[1]=1;
-     nxt_dcache[index].way[1].tag=dcif.dmemaddr[31:6];
-     end
-     dcif.dhit=1;
- end
- nhitR_doneC: begin
-     if(!nxt_dcache[index].ru[0]) begin
-     if(dcif.dmemaddr[2])begin
-     nxt_dcache[index].way[0].data[0]=read_block[0];
-     nxt_dcache[index].way[0].data[1]=dcif.dmemstore;
-     end
-     else begin
-     nxt_dcache[index].way[0].data[1]=read_block[1];
-     nxt_dcache[index].way[0].data[0]=dcif.dmemstore;
-     end
-     nxt_dcache[index].way[0].valid=1;
-     nxt_dcache[index].way[0].dirty=0;
-     nxt_dcache[index].way[0].tag=dcif.dmemaddr[31:6];
-     nxt_dcache[index].ru[0]=1;
-     nxt_dcache[index].ru[1]=0;
-     end
-     else if(!nxt_dcache[index].ru[1]) begin
-     if(dcif.dmemaddr[2])begin
-     nxt_dcache[index].way[1].data[0]=read_block[0];
-     nxt_dcache[index].way[1].data[1]=dcif.dmemstore;
-     end
-     else begin
-     nxt_dcache[index].way[1].data[1]=read_block[1];
-     nxt_dcache[index].way[1].data[0]=dcif.dmemstore;
-     end
-     nxt_dcache[index].way[1].valid=1;
-     nxt_dcache[index].way[1].dirty=0;
-     nxt_dcache[index].way[1].tag=dcif.dmemaddr[31:6];
-     nxt_dcache[index].ru[1]=1;
-     nxt_dcache[index].ru[0]=0;
-     end
-     dcif.dhit=1;
- end
- halt_cleaned: begin
-    cif.dWEN=1;
-    cif.daddr=32'h3100;
-    cif.dstore=halt_cnt;
-    
- end
-
-
- endcase
-
- end
- assign dcif.flushed=(state==halt_cleaned&&!cif.dwait);
 endmodule
